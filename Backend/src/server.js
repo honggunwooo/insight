1127126dd3e165eba8d95bcffa15dbee74cbebb1
honggunwooo <hongkxh@gmail.com>
2025-11-05 19:20:06 +0900

    Update authRoutes and fix DB/user info route

--- Backend/src/server.js
+++ Backend/src/server.js
@@ -4,6 +4,8 @@ const http = require("http");
 const { Server } = require("socket.io");
 require("dotenv").config();
 
+const pool = require("./config/db");
+
 const authRoutes = require("./routes/authRoutes");
 const profileRoutes = require("./routes/profileRoutes");
 const authMiddleware = require("./middlewares/jwt");
@@ -18,6 +20,16 @@ app.use("/uploads", express.static("uploads"));
 app.get("/api/protected", authMiddleware, (req, res) => {
   res.json({ message: "보호된 API 접근 성공", user: req.user });
 });
+
+// DB 연결 테스트
+pool.getConnection()
+  .then(conn => {
+    console.log("✅ DB Connected!");
+    conn.release();
+  })
+  .catch(err => {
+    console.error("❌ DB 연결 실패:", err);
+  });
 
 // 라우터 등록
 app.use("/api/auth", authRoutes);
